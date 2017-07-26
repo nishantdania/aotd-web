089bab640c66a0bc7e68143e8f5bce740245df68
nishantdania <nishantdania@gmail.com>
2017-07-26 13:27:53 +0530

    removed dropzone

--- src/components/UploadWidget/uploadWidget.js
+++ src/components/UploadWidget/uploadWidget.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import cx from 'classnames';
 import styles from './uploadWidget.css'; 
-import Dropzone from 'react-dropzone'
 import TransparentButton from '../Buttons/TransparentButton';
 
 class UploadWidget extends Component {
@@ -10,11 +9,14 @@ class UploadWidget extends Component {
     file: null
   }
 
-  onDrop = (acceptedFiles, rejectedFiles) => {
-    console.log('acceptedFiles: ', acceptedFiles); 
-    if(acceptedFiles.length > 0) {
+  handleChange = (e) => {
+    var files = e.target.files;
+    if(files.length > 0) {
+      var file = files[0];
+      var preview = window.URL.createObjectURL(file);
+      file.preview = preview;
       this.setState({
-        file: acceptedFiles[0]   
+        file
       });
     }
   }
@@ -30,13 +32,17 @@ class UploadWidget extends Component {
     const {file} = this.state;
 
     return <div>
-      {!file ? <Dropzone
-          className={cx(styles['dropzone'])} 
-          multiple={false}
-          accept="image/jpg, image/png, image/jpeg"
-          onDrop={this.onDrop} >
-          <p>Drag and drop image here or <span>browse</span></p>
-        </Dropzone>
+      {!file ?
+        <div className={cx(styles['uploadWrapper'])}>
+          <TransparentButton 
+            text='Upload art'
+            className={cx(styles['uploadButton'])} />
+          <input 
+            type='file'
+            onChange={this.handleChange}
+            className={cx(styles['uploadInput'])}
+          />
+        </div>
       : null}
       {file ?
         <div> 
