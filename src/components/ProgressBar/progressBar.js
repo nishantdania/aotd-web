b872edb727554887e4532f56ce1035258420ccbe
nishantdania <nishantdania@gmail.com>
2017-07-28 08:34:24 +0530

    added progress bar

---
+++ src/components/ProgressBar/progressBar.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import cx from 'classnames';
+import styles from './progressBar.css'; 
+
+class ProgressBar extends Component {
+
+  render () {
+
+    const { xp, level, maxXP, baseXP, nextGoal } = this.props;
+    var maxPercent = (((maxXP-baseXP)/(nextGoal-baseXP))*100).toString() + '%';
+    var curPercent = (((xp-baseXP)/(nextGoal-baseXP))*100).toString() + '%';
+    var maxXPStyle = {
+      width: maxPercent 
+    };
+
+    var curXPStyle = {
+      width: curPercent 
+    };
+
+    return <div className={cx(styles['outer'])}>
+      <div className={cx(styles['progress-wrapper'])}>
+        <div className={cx(styles['progress-base'])} />
+        <div  
+          style={maxXPStyle}
+          className={cx(styles['progress-max'])}
+        />
+        <div className={cx(styles['progress-xp'])} 
+          style={curXPStyle}
+        />
+      </div>
+      <span>
+        <div className={cx(styles['next-level-title'])}>
+          Level {level+1}
+        </div>
+        <div className={cx(styles['next-level-xp'])}>
+          {nextGoal} XP
+        </div>
+      </span>
+    </div>
+  }
+}
+
+export default ProgressBar;
--- src/components/UserScoreWidget/userScoreWidget.js
+++ src/components/UserScoreWidget/userScoreWidget.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import cx from 'classnames';
 import styles from './userScoreWidget.css'; 
+import ProgressBar from '../ProgressBar';
 
 class UserScoreWidget extends Component {
 
@@ -12,13 +13,38 @@ class UserScoreWidget extends Component {
     </div>
   }
 
+  renderPrivateData = (score) => {  
+    const { xp, level, maxXP, baseXP, nextGoal, boosters } = score || {};
+    return <div className={cx(styles['privateScore'])}>
+      <div className={cx(styles['boosters'])}>
+        <span>
+          Boosters available : {boosters}
+        </span>
+        <span className={cx(styles['link'])}>
+          Use Booster
+        </span>
+        <span className={cx(styles['link'])}>
+          Earn Boosters
+        </span>
+      </div>
+      <ProgressBar 
+        level={level}
+        xp={xp}
+        maxXP={maxXP}
+        nextGoal={nextGoal}
+        baseXP={baseXP}
+      />
+    </div>
+  }
+
   render () {
 
     const { score } = this.props;
-    const { xp, level, maxXP, baseXP } = score || {};
+    const { xp, level, maxXP, baseXP, boosters } = score || {};
   
     return <div>
       {this.renderPublicData(score)}
+      {this.renderPrivateData(score)}
     </div> 
   }
 }
